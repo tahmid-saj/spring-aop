dd564b14016bef3b6062a36c110b8d45bef1f314
tahmid-saj <tahmid.saj@gmail.com>
2023-10-09 23:42:30 -0400

    matching on access modifier

--- aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,7 +13,7 @@ public class MyDemoLoggingAspect {
 
     // start with @Before advice
 
-    @Before("execution(public void add*())")
+    @Before("execution(void add*())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
 
