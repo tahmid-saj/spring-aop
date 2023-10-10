0a76d389e4668a1c62b60f59980ef65bf01ec66a
tahmid-saj <tahmid.saj@gmail.com>
2023-10-09 23:33:51 -0400

    membership dao

--- aopdemo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ aopdemo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,6 +1,7 @@
 package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,19 +15,22 @@ public class AopdemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 		return runner -> {
-			demoTheBeforeAdvice(theAccountDAO);
+			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
 		};
 	}
 
-	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 		// call the business method
 		theAccountDAO.addAccount();
 
-		// do it again
-		System.out.println("calling it again");
-		theAccountDAO.addAccount();
+		// call the membership business method
+		theMembershipDAO.addAccount();
+
+//		// do it again
+//		System.out.println("calling it again");
+//		theAccountDAO.addAccount();
 	}
 
 }
---
+++ aopdemo/src/main/java/com/luv2code/aopdemo/dao/MembershipDAOImpl.java
@@ -0,0 +1,12 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MembershipDAOImpl implements MembershipDAO {
+
+    @Override
+    public void addAccount() {
+        System.out.println(getClass() + ": Doing my DB work, adding a membership");
+    }
+}
