46548c6a061d50e9d813162822e403228b308cf2
tahmid-saj <tahmid.saj@gmail.com>
2023-10-09 22:57:33 -0400

    before advice

---
+++ aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,24 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+    // where we add all of the related advices for logging
+
+
+    // start with @Before advice
+
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n====>>> Executing @Before advice on addAccount()");
+
+
+    }
+}
+
+
