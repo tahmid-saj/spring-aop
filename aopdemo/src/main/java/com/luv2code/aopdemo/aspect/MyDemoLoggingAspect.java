8e97534a7e4e17e929c31463b96adc0cff240e77
tahmid-saj <tahmid.saj@gmail.com>
2023-10-09 23:37:10 -0400

    only matching on account dao

--- aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,7 +13,7 @@ public class MyDemoLoggingAspect {
 
     // start with @Before advice
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
 
